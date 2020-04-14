8837835b0a3d3f9c9323109e30207163eab29a90
Hideraldo Luís <hideraldus13@gmail.com>
2020-04-13 21:02:32 -0300

    Retirada do package-lock.json

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const graphqlHTTP = require('express-graphql')
 const express = require('express')
 const usuarios = require('./schema')
 const app = express()
+
 app.use('/teste', graphqlHTTP({schema:usuarios, pretty: true}))
+
 app.listen(3000, function () {
   console.log('Server on.')
 })
